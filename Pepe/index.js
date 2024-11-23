0adcfd1873fe59d757fac9cfda09d49e165d9e47
Andreas <jw@gbs.wiki>
2024-11-23 23:49:52 +0900

    Add files via upload

---
+++ Pepe/index.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+
+    let imgs = document.querySelectorAll('img');
+    imgs.forEach((a, i) => {
+    a.src = "https://dszw1qtcnsa5e.cloudfront.net/community/20200910/7988ee0e-3de7-4152-99a1-0a3aeebc6244/6E365727B1E2416BA4EA8E57A0C8F5BC.gif"
+    })
+
+}, 500)
